8099eb9b54adcac8c30cfeb553d1c5e04bfd66bb
Houssem Dellai <houssem.dellai@soat.fr>
2018-10-14 09:07:37 +0200

    Add project files.

---
+++ BankAccountLib/BankAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace BankAccountLib
+{
+    /// <summary>
+    /// Bank Account demo class.
+    /// src: https://docs.microsoft.com/en-us/visualstudio/test/sample-project-for-creating-unit-tests?view=vs-2017
+    /// </summary>
+    public class BankAccount
+    {
+        private string _customerName;
+        private double _balance;
+        private bool _frozen = false;
+
+        public string CustomerName
+        {
+            get { return _customerName; }
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            _balance = balance;
+        }
+
+        public void Debit(double amount)
+        {
+            if (_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount > _balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            _balance += amount; // intentionally incorrect code
+        }
+
+        public void Credit(double amount)
+        {
+            if (_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            _balance += amount;
+        }
+
+        private void FreezeAccount()
+        {
+            _frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            _frozen = false;
+        }
+    }
+}
