b76f1e430d9bcc88792c01a4ee3abfad8b9299f2
Houssem Dellai <houssem.dellai@soat.fr>
2018-11-11 11:34:32 +0100

    Added tests using Moq and Bogus

--- ProductsStore.UnitTests/ProductsServiceWithMockTests.cs
+++ ProductsStore.UnitTests/ProductsServiceWithMockTests.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using Moq;
 using ProductsStore.Models;
 using ProductsStore.Services;
@@ -11,21 +12,21 @@ namespace ProductsStore.UnitTests
     public class ProductsServiceTestsWithMock
     {
         private readonly Mock<IProductsRepository> _mockProductsRepository;
+        private Product _product;
+        private IEnumerable<Product> _products;
 
         public ProductsServiceTestsWithMock()
         {
             // TODO: generate sample products using Bogus
 
-            var product = new Product
+            _product = new Product
             {
                 Id = 1,
                 Name = "Laptop"
             };
-            var products = new List<Product>();
-            products.Add(product);
-            _mockProductsRepository = new Mock<IProductsRepository>();
+            _products = new List<Product> { _product };
 
-            //_fakeProductsRepository = new FakeProductsRepository(products);
+            _mockProductsRepository = new Mock<IProductsRepository>();
         }
 
         [Fact]
@@ -33,7 +34,99 @@ namespace ProductsStore.UnitTests
         public async Task GetAllProducts_ReturnsAvailableProducts()
         {
             // Arrange
-            IEnumerable<Product> products = new List<Product> { new Product { Id = 1, Name = "Laptop" } };
+            _mockProductsRepository.Setup(x => x.GetAllProducts()).Returns(Task.FromResult(_products));
+            
+            var sut = new ProductsService(_mockProductsRepository.Object);
+
+            // Act
+            var actual = await sut.GetAllProducts();
+
+            // Assert
+            Assert.True(actual.ToList().Count > 0);
+        }
+
+        [Fact]
+        [Trait("ProductsStore", "GetById")]
+        public async Task GetById_ValidId_ReturnsProduct()
+        {
+            // Arrange
+            _mockProductsRepository.Setup(x => x.GetById(1)).Returns(Task.FromResult(_product));
+
+            var sut = new ProductsService(_mockProductsRepository.Object);
+
+            // Act
+            var actual = await sut.GetById(1);
+
+            // Assert
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        [Trait("ProductsStore", "GetById")]
+        public async Task GetById_NegativeId_ReturnsNull()
+        {
+            // Arrange
+            _mockProductsRepository.Setup(x => x.GetById(-1)).Returns(Task.FromResult<Product>(null));
+
+            var sut = new ProductsService(_mockProductsRepository.Object);
+
+            // Act
+            var actual = await sut.GetById(-1);
+
+            // Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        [Trait("ProductsStore", "Add")]
+        public async Task Add_ValidProduct_ReturnsProduct()
+        {
+            // Arrange
+            _mockProductsRepository.Setup(x => x.Add(_product)).Returns(Task.FromResult(_product));
+
+            var sut = new ProductsService(_mockProductsRepository.Object);
+
+            // Act
+            var actual = await sut.Add(_product);
+
+            // Assert
+            Assert.True(actual.Id == 1);
+            Assert.True(actual.Name == "Laptop");
+        }
+
+        [Fact]
+        [Trait("ProductsStore", "Add")]
+        public async Task Add_EmptyName_ReturnsNull()
+        {
+            // Arrange
+            _mockProductsRepository.Setup(x => x.Add(_product)).Returns(Task.FromResult(_product));
+
+            var sut = new ProductsService(_mockProductsRepository.Object);
+
+            // Act
+            var actual = await sut.Add(_product);
+
+            // Assert
+            Assert.True(actual.Id == 1);
+            Assert.True(actual.Name == "Laptop");
+        }
+
+        [Fact]
+        [Trait("ProductsStore", "GetAllProducts")]
+        public async Task GetAllProducts_ReturnsAvailableProducts_UsingBogus()
+        {
+            // Arrange
+            var id = 1;
+            var names = new[] { "laptop", "xbox", "TV", "smartphone", "tablet" };
+            var faker = new Faker<Product>()
+                              .RuleFor(p => p.Id, f => id++)
+                              .RuleFor(p => p.Name, f => f.PickRandom(names));
+
+            IEnumerable<Product> products = faker.Generate(100);
+
+            //var faker2 = new Faker<Product>()
+            //                   .RuleFor(p => p.Id, f => f.Random.Int())
+            //                   .RuleFor(p => p.Name, f => f.Random.String());
 
             _mockProductsRepository.Setup(x => x.GetAllProducts()).Returns(Task.FromResult(products));
 
@@ -45,38 +138,5 @@ namespace ProductsStore.UnitTests
             // Assert
             Assert.True(actual.ToList().Count > 0);
         }
-
-        //[Fact]
-        //[Trait("ProductsStore", "GetById")]
-        //public async Task GetById_ValidId_ReturnsProduct()
-        //{
-        //    // Arrange
-        //    var sut = new ProductsService(_mockProductsRepository);
-
-        //    // Act
-        //    var actual = await sut.GetById(1);
-
-        //    // Assert
-        //    Assert.NotNull(actual);
-        //}
-
-        //[Fact]
-        //[Trait("ProductsStore", "Add")]
-        //public async Task Add_ValidId_ReturnsProduct()
-        //{
-        //    // Arrange
-        //    var sut = new ProductsService(_mockProductsRepository);
-
-        //    // Act
-        //    var actual = await sut.Add(new Product
-        //    {
-        //        Id = 2,
-        //        Name = "Smartphone"
-        //    });
-
-        //    // Assert
-        //    Assert.True(actual.Id == 2);
-        //    Assert.True(actual.Name == "Smartphone");
-        //}
     }
 }
